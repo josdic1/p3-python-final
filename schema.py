d92af8f10f4ccff1d1beee40d00cd14792a71786
josdic1 <joshuadicker@gmail.com>
2025-08-20 10:40:56 -0400

    created schema with tables - groups, restaurants

--- schema.py
+++ schema.py
@@ -3,9 +3,24 @@ import sqlite3
 CONN = sqlite3.connect('database.db')
 CURSOR = CONN.cursor()
 
+SCHEMA = """
 
+CREATE TABLE IF NOT EXISTS groups (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
 
-CURSOR.executescript()
+CREATE TABLE IF NOT EXISTS restaurants (
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    location TEXT NULL,
+    group_id INTEGER,
+    FOREIGN KEY (group_id) REFERENCES groups.id
+);
+
+"""
+
+CURSOR.executescript(SCHEMA)
 CONN.commit()
 CONN.close()
 
