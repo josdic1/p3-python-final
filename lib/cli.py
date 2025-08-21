cf222e597dc244e5ae027b6d3ff65d0cdebc5452
josdic1 <joshuadicker@gmail.com>
2025-08-21 09:53:31 -0400

    building cli menue for group class

--- lib/cli.py
+++ lib/cli.py
@@ -47,7 +47,7 @@ def groups_menu():
             view_group()
 
 def create_group():
-    pass
+    print("create")
 
 def delete_group():
     pass
@@ -58,88 +58,88 @@ def view_group():
 
     
 
-    # choice = input("\nEnter group id to view, N to create new group, or B to go back: ")
+#     # choice = input("\nEnter group id to view, N to create new group, or B to go back: ")
 
-    # if choice.upper() == "B":
-    #     return
+#     # if choice.upper() == "B":
+#     #     return
 
-    # elif choice.upper() == "N":
-    #     print("\n=== New Restaurant Group ===")
-    #     name = input("Enter new group name: ")
+#     # elif choice.upper() == "N":
+#     #     print("\n=== New Restaurant Group ===")
+#     #     name = input("Enter new group name: ")
 
-    #     new_group = RestGroup.create(name)
-    #     print(f"\nCreated new group: {new_group.id} - {new_group.name}")
+#     #     new_group = RestGroup.create(name)
+#     #     print(f"\nCreated new group: {new_group.id} - {new_group.name}")
         
-    # elif choice.isdigit():
-    #     group = RestGroup.find_by_id(int(choice))
-    #     if group:
-    #         restaurants_in_group(group)
-    #     else:
-    #         print("No group found with that id.")
-    # else:
-    #     print("Invalid choice")
+#     # elif choice.isdigit():
+#     #     group = RestGroup.find_by_id(int(choice))
+#     #     if group:
+#     #         restaurants_in_group(group)
+#     #     else:
+#     #         print("No group found with that id.")
+#     # else:
+#     #     print("Invalid choice")
 
 
-def restaurants_in_group(group):
-    print(f"\n=== Restaurants in {group.name} ===")
-    for r in group.restaurants():
-        print(f"{r.id}: {r.name}")
+# def restaurants_in_group(group):
+#     print(f"\n=== Restaurants in {group.name} ===")
+#     for r in group.restaurants():
+#         print(f"{r.id}: {r.name}")
 
-    choice = input("Enter restaurant id to view, N to create/assign restaurant to this group, or B to go back: ")
+#     choice = input("Enter restaurant id to view, N to create/assign restaurant to this group, or B to go back: ")
 
-    if choice.upper() == "B":
-        return
+#     if choice.upper() == "B":
+#         return
     
-    elif choice.upper() == "N":
-        print("\n=== New Restaurant ===")
-        name = input("Enter new restaurant name: ")
-        location = input("Enter new restaurant location: ")
+#     elif choice.upper() == "N":
+#         print("\n=== New Restaurant ===")
+#         name = input("Enter new restaurant name: ")
+#         location = input("Enter new restaurant location: ")
         
-        new_restaurant = Restaurant.create(name, location, group.id)
-        print(f"\nCreated new restaurant: {new_restaurant.id} - {new_restaurant.name} in {new_restaurant.location} for {group.name}")
+#         new_restaurant = Restaurant.create(name, location, group.id)
+#         print(f"\nCreated new restaurant: {new_restaurant.id} - {new_restaurant.name} in {new_restaurant.location} for {group.name}")
         
-    elif choice.isdigit():
-        selected = Restaurant.find_by_id(int(choice))
-        if selected:
-            show_restaurant(selected)
-        else:
-            print("No restaurant found with that id.")
-    else:
-        print("Invalid choice")
+#     elif choice.isdigit():
+#         selected = Restaurant.find_by_id(int(choice))
+#         if selected:
+#             show_restaurant(selected)
+#         else:
+#             print("No restaurant found with that id.")
+#     else:
+#         print("Invalid choice")
 
 
-def show_restaurant(restaurant):
-    print(f"\n--- Restaurant ---")
-    print(f"ID: {restaurant.id}")
-    print(f"Name: {restaurant.name}")
-    input("\nPress Enter to go back...")
+# def show_restaurant(restaurant):
+#     print(f"\n--- Restaurant ---")
+#     print(f"ID: {restaurant.id}")
+#     print(f"Name: {restaurant.name}")
+#     input("\nPress Enter to go back...")
 
 
-def restaurants_menu():
-    print("\n=== All Restaurants ===")
-    all = Restaurant.get_all()
-    for r in all:
-        print(f"{r.id}: {r.name}")
+# def restaurants_menu():
+#     print("\n=== All Restaurants ===")
+#     all = Restaurant.get_all()
+#     for r in all:
+#         print(f"{r.id}: {r.name}")
 
-    choice = input("Enter restaurant id to view, or B to go back: ")
+#     choice = input("Enter restaurant id to view, or B to go back: ")
 
-    if choice.upper() == "B":
-        return
-    elif choice.isdigit():
-        restaurant = Restaurant.find_by_id(int(choice))
-        if restaurant:
-            restaurant_selection(restaurant)
-        else:
-            print("No restaurant found with that ID.")
-    else:
-        print("Invalid choice")
+#     if choice.upper() == "B":
+#         return
+#     elif choice.isdigit():
+#         restaurant = Restaurant.find_by_id(int(choice))
+#         if restaurant:
+#             restaurant_selection(restaurant)
+#         else:
+#             print("No restaurant found with that ID.")
+#     else:
+#         print("Invalid choice")
 
 
-def restaurant_selection(restaurant):
-    print(f"\n--- Restaurant ---")
-    print(f"ID: {restaurant.id}")
-    print(f"Name: {restaurant.name}")
-    input("\nPress Enter to go back...")
+# def restaurant_selection(restaurant):
+#     print(f"\n--- Restaurant ---")
+#     print(f"ID: {restaurant.id}")
+#     print(f"Name: {restaurant.name}")
+#     input("\nPress Enter to go back...")
 
 
 if __name__ == "__main__":
