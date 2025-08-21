f000cad478b427344adbd32a60d7716bdd564bc0
josdic1 <joshuadicker@gmail.com>
2025-08-21 14:01:31 -0400

    added drop_table OR delete_table method

--- drop_tables.py
+++ drop_tables.py
@@ -3,12 +3,18 @@ import sqlite3
 CONN = sqlite3.connect("database.db")
 CURSOR = CONN.cursor()
 
-SQL = """
+SQL_DROP = """
 DROP TABLE IF EXISTS restaurants;
 DROP TABLE IF EXISTS groups;
 """
 
-CURSOR.executescript(SQL)
+SQL_DELETE = """
+CURSOR.execute("DELETE FROM restaurants")
+CURSOR.execute("DELETE FROM rest_groups")
+CONN.commit()
+"""
+
+CURSOR.executescript(SQL_DROP)
 CONN.commit()
 
 print("❌ Tables dropped.")
